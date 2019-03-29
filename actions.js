ed72f3c06ded6ef97e67567d46c1a9f5442d200a
i.ustinchenko <i.ustinchenko@betinvest.com>
2019-03-29 11:47:48 +0200

    003

--- actions.js
+++ actions.js
@@ -1,8 +1,10 @@
 import fetch from 'isomorphic-fetch';
+import { takeEvery, put, call } from 'redux-saga/effects';
 
 export const REQUESTED_DOG = 'REQUESTED_DOG';
 export const REQUESTED_DOG_SUCCEEDED = 'REQUESTED_DOG_SUCCEEDED';
 export const REQUESTED_DOG_FAILED = 'REQUESTED_DOG_FAILED';
+export const FETCHED_DOG = 'FETCHED_DOG';
 
 const requestDog = () => {
     return { type: REQUESTED_DOG }
@@ -12,18 +14,42 @@ const requestDogSuccess = (data) => {
     return { type: REQUESTED_DOG_SUCCEEDED, url: data.message }
 };
 
-const requestDogaError = () => {
+const requestDogError = () => {
     return { type: REQUESTED_DOG_FAILED }
 };
 
+// export const fetchDog = () => {
+//     return (dispatch) => {
+//         dispatch(requestDog());
+//         fetch('https://dog.ceo/api/breeds/image/random')
+//             .then(res => res.json())
+//             .then(
+//                 data => dispatch(requestDogSuccess(data)),
+//                 err => dispatch(requestDogError(err))
+//             );
+//     }
+// };
+
 export const fetchDog = () => {
-    return (dispatch) => {
-        dispatch(requestDog());
-        fetch('https://dog.ceo/api/breeds/image/random')
-            .then(res => res.json())
-            .then(
-                data => dispatch(requestDogSuccess(data)),
-                err => dispatch(requestDogaError(err))
-            );
-    }
+    return { type: FETCHED_DOG }
 };
+
+function* fetchDogAsync() {
+    try {
+        yield put(requestDog());
+        const data = yield call(() => {
+                return fetch('https://dog.ceo/api/breeds/image/random')
+                    .then(res => res.json())
+            }
+        );
+        yield put(requestDogSuccess(data));
+    } catch (error) {
+        yield put(requestDogError());
+    }
+}
+
+export function* watchFetchDog() {
+    yield takeEvery(FETCHED_DOG, fetchDogAsync);
+}
+
+
--- main.js
+++ main.js
@@ -5,22 +5,33 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import { createStore, applyMiddleware } from 'redux'
-import { Provider, connect} from 'react-redux';
+import { Provider } from 'react-redux';
 
 import  reducer from './reducers';
 
-import thunk from 'redux-thunk';
+//import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { watchFetchDog } from './actions';
 
 
 
 //const store = createStore(reducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let initialState = {};
+// const store = createStore(
+//     reducer,
+//     initialState,
+//     composeEnhancers(applyMiddleware(thunk))
+// );
+
+const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     reducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+sagaMiddleware.run(watchFetchDog);
 
 // Container component
 ReactDOM.render(
